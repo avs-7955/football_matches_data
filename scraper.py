1b0ee8085cddde8e4fffc364b0da93e1fb23b199
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-12-08 17:17:58 +0530

    Add comments

--- scraper.py
+++ scraper.py
@@ -2,18 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+# Website containing data about Football Matches
 website = 'https://www.adamchoi.co.uk/teamgoals/detailed'
+
+# Chrome Driver path
 path = 'D:\VIT\Self_Dev\CODING\Web Scraping\chromedriver'
 
 driver = webdriver.Chrome(path)
 driver.get(website)
 
+# Clicking the all matches btn
 all_matches_btn = driver.find_element(By.XPATH,
                                       '//label[@analytics-event="All matches"]')
 all_matches_btn.click()
 
-
+# Finding the rows of the table
 matches = driver.find_elements(By.TAG_NAME, 'tr')
+
+# Creating empty lists and appending items to it
 date = []
 home_team = []
 score = []
@@ -26,10 +32,7 @@ for match in matches:
     away_team.append(match.find_element(By.XPATH, './td[4]').text)
 driver.quit()
 
+# Creating a data frame and stroring the data into excel file
 df = pd.DataFrame({'Date': date, 'HomeTeam': home_team,
                   'Score': score, 'AwayTeam': away_team})
 df.to_excel('football_scores.xlsx', 'All matches', index=False)
-# df.to_csv('football_matches', index=False)
-
-
-# time.sleep(5000)
